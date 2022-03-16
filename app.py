21b484cbbc1c3983bf9def03a9fb472e9e49eea5
venkatesh <venkateshgudapati@gmail.com>
2022-03-16 14:10:11 +0530

    resolved conflicts in cherry-pick

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
+<<<<<<< HEAD
 this is a pyton file
 no it is a text file
+=======
+this is a pyton file!
+are you sure?
+yes, iam
+But No it is not a python file
+>>>>>>> 0d52cf3 (added info in app.py)
